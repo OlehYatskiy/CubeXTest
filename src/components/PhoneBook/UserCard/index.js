3f19592e67dbcb231c3000616f63ee7c9ef10cb8
OlehYatskiy <nor4yk2@gmail.com>
2019-01-14 18:36:16 +0200

    add at UserCard EditButton, add at UserList SearchInput and deleteUserButton

--- src/components/PhoneBook/UserCard/index.js
+++ src/components/PhoneBook/UserCard/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { string } from "prop-types";
 import classNames from "classnames";
 
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Image, Segment, Button } from 'semantic-ui-react'
 
 // import classes from './index.less';
 
@@ -18,7 +18,13 @@ class UserCard extends Component {
         <Card>
           <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
           <Card.Content>
-            <Card.Header>Matthew</Card.Header>
+            <Card.Header>Current Name</Card.Header>
+            <Segment.Group>
+              <Segment>current phone number</Segment>
+              <Segment>current email</Segment>
+              <Segment>current company</Segment>
+            </Segment.Group>
+            <Button floated='right' content='Edit' icon='pencil alternate' labelPosition='left'/>
           </Card.Content>
         </Card>
       )}
--- src/components/PhoneBook/UsersList/index.js
+++ src/components/PhoneBook/UsersList/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { string } from "prop-types";
-import { List, Image } from 'semantic-ui-react';
+import {
+  List,
+  Image,
+  Container,
+  Input,
+  Button
+} from 'semantic-ui-react';
 
 import classNames from "classnames";
 
@@ -15,20 +21,28 @@ class UsersList extends Component {
     render() {
 
         return (
-          <List selection verticalAlign='middle'>
-            <List.Item>
-              <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
-              <List.Content>
-                <List.Header>Helen</List.Header>
-              </List.Content>
-            </List.Item>
-            <List.Item>
-              <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
-              <List.Content>
-                <List.Header>Christian</List.Header>
-              </List.Content>
-            </List.Item>
-          </List>
+          <Container>
+            <Input
+              icon={{ name: 'search', circular: true, link: true }}
+              placeholder='Search...' >
+              {/*Search input value*/}
+            </Input>
+            <List selection verticalAlign='middle'>
+              <List.Item>
+                <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
+                <List.Content>
+                  <List.Header>Helen</List.Header>
+                </List.Content>
+                <Button floated='right' icon='delete'/>
+              </List.Item>
+              <List.Item>
+                <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
+                <List.Content>
+                  <List.Header>Christian</List.Header>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Container>
     )
   }
 }
